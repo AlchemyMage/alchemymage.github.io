0b04e8023d8f17377a4aebde9ec75e5730444890
AlchemyMage <alchemy@posetmage.com>
2024-02-17 14:38:56 +0800

    upload

--- upload.ps1
+++ upload.ps1
@@ -6,6 +6,10 @@ cd _layouts
 ./upload.ps1
 cd ../
 
+cd assets
+./upload.bat
+cd ../
+
 git config --local user.name "AlchemyMage"
 git config --local user.email "alchemy@posetmage.com"
 git remote set-url origin git@AM:AlchemyMage/AlchemyMage.github.io.git
