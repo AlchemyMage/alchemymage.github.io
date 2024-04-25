a5e1fa5be3e6fc49ae52c61e49dedf64d9fcc457
AlchemyMage <alchemy@posetmage.com>
2024-04-25 17:50:25 +0800

    AL

--- upload.ps1
+++ upload.ps1
@@ -12,7 +12,7 @@ cd ../
 
 git config --local user.name "AlchemyMage"
 git config --local user.email "alchemy@posetmage.com"
-git remote set-url origin git@AM:AlchemyMage/AlchemyMage.github.io.git
+git remote set-url origin git@AL:AlchemyMage/AlchemyMage.github.io.git
 
 git submodule update --recursive --remote
 
